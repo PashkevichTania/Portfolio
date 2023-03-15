0d3f1ffd6956e16c1065363a9152e3978bd5eba9
PashkevichTania <PashkevichTania>
2023-03-15 12:26:41 +0300

    refactor revalidate.ts

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,20 +1,31 @@
-import { isValidRequest } from "@sanity/webhook"
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
+  success: boolean
   message: string
 }
 
 const secret = process.env.NEXT_SANITY_SECRET || ""
 
+async function readBody(readable: NextApiRequest) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString("utf8")
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method !== "POST") {
     console.error("Must be a POST request")
-    return res.status(401).json({ message: "Must be a POST request" })
+    return res.status(405).json({ success: false, message: "Must be a POST request" })
   }
 
-  if (!isValidRequest(req, secret)) {
-    res.status(401).json({ message: "Invalid signature" })
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string
+  const body = await readBody(req) // Read the body into a string
+  if (!isValidSignature(body, signature, secret)) {
+    res.status(401).json({ success: false, message: "Invalid signature" })
     return
   }
 
@@ -26,13 +37,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     switch (type) {
       case "post":
         const pathToRevalidate = req.body.slug.current
-        console.log(`===== Revalidating: ${pathToRevalidate}`)
+        console.log(`==== Revalidating: ${pathToRevalidate} =====`)
         await res.revalidate(pathToRevalidate)
-        return res.json({ message: `Revalidated "${type}" with slug "${slug}"` })
+        return res.json({ success: true, message: `Revalidated "${type}" with slug "${slug}"` })
     }
 
-    return res.json({ message: "No managed type" })
+    return res.json({ success: false, message: "No managed type" })
   } catch (err) {
-    return res.status(500).send({ message: "Error revalidating" })
+    return res.status(500).send({ success: false, message: "Error revalidating" })
   }
 }
